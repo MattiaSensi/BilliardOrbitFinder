2f5d6118080f3674dca756585834787df45cd499
MattiaSensi <74617398+MattiaSensi@users.noreply.github.com>
2024-08-22 09:50:13 +0200

    Add files via upload

--- billiards_NBO_manu.m
+++ billiards_NBO_manu.m
@@ -2,17 +2,17 @@
 %https://arxiv.org/abs/AAAA.BBBBB
 
 %a and b are the semi-axes of the ellipse
-a=5;   
+a=5;
 b=4.5;
 
 %parametrization of the ellipse; \theta=2*\pi*t so that the table
 %is parametrized for t \in [0,1).
-Gamma1 = @(t,a,b) a*cos(2*pi*t);   
+Gamma1 = @(t,a,b) a*cos(2*pi*t);
 Gamma2 = @(t,a,b) b*sin(2*pi*t);
 
-%p and N to define a (Np,Nq) orbit, starting from an (Np,Nq) state. 
+%p and N to define a (Np,Nq) orbit, starting from an (Np,Nq) state.
 %q is determined by the initial conditions.
-p=9;
+p=7;
 N=2;
 
 %x contains the initial conditions for the gradient flow; we are working
@@ -22,7 +22,7 @@ x=zeros(N*p,1);
 
 %here we use formula (XX) from the manuscript
 for ii=1:N*p
-    x(ii)=(-1)^(ii+1)*(1/4)+sin(4*pi*(ii-1/2)/(N*p))/5; 
+    x(ii)=(-1)^(ii+1)*(1/4)+sin(4*pi*(ii-1/2)/(N*p))/5;
 end
 
 %standard forward integration of the dynamical system with ode45
@@ -35,16 +35,16 @@ tspan = [0 1000];
 x=X(end,:);
 
 %discretization to plot the ellipse
-howmany=2000;   
+howmany=2000;
 
 %lines 44-58 plot the orbit corresponding to the given initial condition in cyan
 figure
 
-hold on 
+hold on
 ttime=linspace(0,1,howmany);
 
 plot(Gamma1(ttime,a,b),Gamma2(ttime,a,b),'Color','k','LineWidth',1)
-for jj=1:length(x)-1  
+for jj=1:length(x)-1
     line([Gamma1(x0(jj),a,b) Gamma1(x0(jj+1),a,b)],[Gamma2(x0(jj),a,b) Gamma2(x0(jj+1),a,b)],'Color','c','LineWidth',1.5);
 end
 line([Gamma1(x0(end),a,b) Gamma1(x0(1),a,b)],[Gamma2(x0(end),a,b) Gamma2(x0(1),a,b)],'Color','c','LineWidth',1.5)
@@ -54,15 +54,15 @@ set(gcf,'color','w');
 
 hold off
 
-%lines 62-76 plot the orbit corresponding to the final orbit, 
+%lines 62-76 plot the orbit corresponding to the final orbit,
 %an approximation of the desired NBO, in blue
 figure
 
-hold on 
+hold on
 ttime=linspace(0,1,howmany);
 
 plot(Gamma1(ttime,a,b),Gamma2(ttime,a,b),'Color','k','LineWidth',1)
-for jj=1:length(x)-1  
+for jj=1:length(x)-1
     line([Gamma1(x(jj),a,b) Gamma1(x(jj+1),a,b)],[Gamma2(x(jj),a,b) Gamma2(x(jj+1),a,b)],'Color','b','LineWidth',1.5);
 end
 line([Gamma1(x(end),a,b) Gamma1(x(1),a,b)],[Gamma2(x(end),a,b) Gamma2(x(1),a,b)],'Color','b','LineWidth',1.5)
@@ -90,14 +90,14 @@ dydt(N)= HH(X(N-1),X(N),a,b)+HH(X(1),X(N),a,b);
 end
 
 function Acca = HH(A,B,a,b)
-    %the same parametrization given in lines 7 and 8
-    Gamma1 = @(t,a,b) a*cos(2*pi*t);
-    Gamma2 = @(t,a,b) b*sin(2*pi*t);
-    
-    %derivative of Gamma1, Gamma2 with respect to \theta=2*pi*t
-    G1 = @(t,a,b) -a*sin(2*pi*t);
-    G2 = @(t,a,b) b*cos(2*pi*t);
-    
-    %function H as in the paper, equation XXX
-    Acca=(1/(sqrt((Gamma1(A,a,b)-Gamma1(B,a,b))^2+(Gamma2(A,a,b)-Gamma2(B,a,b))^2)))*(G1(B,a,b)*(Gamma1(B,a,b)-Gamma1(A,a,b)) +G2(B,a,b)*(Gamma2(B,a,b)-Gamma2(A,a,b)));
+%the same parametrization given in lines 10 and 11
+Gamma1 = @(t,a,b) a*cos(2*pi*t);
+Gamma2 = @(t,a,b) b*sin(2*pi*t);
+
+%derivative of Gamma1, Gamma2 with respect to \theta=2*pi*t
+G1 = @(t,a,b) -a*sin(2*pi*t);
+G2 = @(t,a,b) b*cos(2*pi*t);
+
+%function H as in the paper, equation XXX
+Acca=(1/(sqrt((Gamma1(A,a,b)-Gamma1(B,a,b))^2+(Gamma2(A,a,b)-Gamma2(B,a,b))^2)))*(G1(B,a,b)*(Gamma1(B,a,b)-Gamma1(A,a,b)) +G2(B,a,b)*(Gamma2(B,a,b)-Gamma2(A,a,b)));
 end
--- billiards_NBO_timerev.m
+++ billiards_NBO_timerev.m
@@ -2,18 +2,18 @@
 %https://arxiv.org/abs/AAAA.BBBBB
 
 %a and b are the semi-axes of the ellipse
-a=5;   
+a=5;
 b=4;
 
 %parametrization of the ellipse; \theta=2*\pi*t so that the table
 %is parametrized for t \in [0,1).
-Gamma1 = @(t,a,b) a*cos(2*pi*t);   
+Gamma1 = @(t,a,b) a*cos(2*pi*t);
 Gamma2 = @(t,a,b) b*sin(2*pi*t);
 
-%p and N to define a (Np,Nq) orbit, starting from an (Np,Nq) state. 
+%p and N to define a (Np,Nq) orbit, starting from an (Np,Nq) state.
 %q is determined by the initial conditions.
 p=2;
-N=12;
+N=4;
 
 %x contains the initial conditions for the gradient flow; we are working
 %with the values t which will then be evaluated in Gamma1 and Gamma2,
@@ -38,16 +38,16 @@ tspan = [0 3000];
 x=X(end,:);
 
 %discretization to plot the ellipse
-howmany=2000;   
+howmany=2000;
 
 %lines 44-58 plot the orbit corresponding to the given initial condition in cyan
 figure
 
-hold on 
+hold on
 ttime=linspace(0,1,howmany);
 
 plot(Gamma1(ttime,a,b),Gamma2(ttime,a,b),'Color','k','LineWidth',1)
-for jj=1:length(x)-1  
+for jj=1:length(x)-1
     line([Gamma1(x0(jj),a,b) Gamma1(x0(jj+1),a,b)],[Gamma2(x0(jj),a,b) Gamma2(x0(jj+1),a,b)],'Color','c','LineWidth',1.5);
 end
 line([Gamma1(x0(end),a,b) Gamma1(x0(1),a,b)],[Gamma2(x0(end),a,b) Gamma2(x0(1),a,b)],'Color','c','LineWidth',1.5)
@@ -57,15 +57,15 @@ set(gcf,'color','w');
 
 hold off
 
-%lines 62-76 plot the orbit corresponding to the final orbit, 
+%lines 62-76 plot the orbit corresponding to the final orbit,
 %an approximation of the desired NBO, in blue
 figure
 
-hold on 
+hold on
 ttime=linspace(0,1,howmany);
 
 plot(Gamma1(ttime,a,b),Gamma2(ttime,a,b),'Color','k','LineWidth',1)
-for jj=1:length(x)-1  
+for jj=1:length(x)-1
     line([Gamma1(x(jj),a,b) Gamma1(x(jj+1),a,b)],[Gamma2(x(jj),a,b) Gamma2(x(jj+1),a,b)],'Color','b','LineWidth',1.5);
 end
 line([Gamma1(x(end),a,b) Gamma1(x(1),a,b)],[Gamma2(x(end),a,b) Gamma2(x(1),a,b)],'Color','b','LineWidth',1.5)
@@ -94,14 +94,14 @@ dydt(N)= HH(X(N-1),X(N),a,b)+HH(X(1),X(N),a,b);
 end
 
 function Acca = HH(A,B,a,b)
-    %the same parametrization given in lines 7 and 8
-    Gamma1 = @(t,a,b) a*cos(2*pi*t);
-    Gamma2 = @(t,a,b) b*sin(2*pi*t);
-    
-    %derivative of Gamma1, Gamma2 with respect to \theta=2*pi*t
-    G1 = @(t,a,b) -a*sin(2*pi*t);
-    G2 = @(t,a,b) b*cos(2*pi*t);
-    
-    %function H as in the paper, equation XXX
-    Acca=(1/(sqrt((Gamma1(A,a,b)-Gamma1(B,a,b))^2+(Gamma2(A,a,b)-Gamma2(B,a,b))^2)))*(G1(B,a,b)*(Gamma1(B,a,b)-Gamma1(A,a,b)) +G2(B,a,b)*(Gamma2(B,a,b)-Gamma2(A,a,b)));
+%the same parametrization given in lines 10 and 11
+Gamma1 = @(t,a,b) a*cos(2*pi*t);
+Gamma2 = @(t,a,b) b*sin(2*pi*t);
+
+%derivative of Gamma1, Gamma2 with respect to \theta=2*pi*t
+G1 = @(t,a,b) -a*sin(2*pi*t);
+G2 = @(t,a,b) b*cos(2*pi*t);
+
+%function H as in the paper, equation XXX
+Acca=(1/(sqrt((Gamma1(A,a,b)-Gamma1(B,a,b))^2+(Gamma2(A,a,b)-Gamma2(B,a,b))^2)))*(G1(B,a,b)*(Gamma1(B,a,b)-Gamma1(A,a,b)) +G2(B,a,b)*(Gamma2(B,a,b)-Gamma2(A,a,b)));
 end
